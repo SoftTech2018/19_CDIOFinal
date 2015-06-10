4fb78235413c21f5c7aa49462757b485e1fff867
JacobWorckJepsen <JacobWorckJepsen@10.16.174.200>
2015-06-10 11:01:39 +0200

    Tilføjelse: updateStatus tilføjet til interface :3

--- 19_CDIOFinal/src/cdio/server/DAL/idao/IProduktBatchDAO.java
+++ 19_CDIOFinal/src/cdio/server/DAL/idao/IProduktBatchDAO.java
@@ -10,4 +10,5 @@ public interface IProduktBatchDAO {
 	List<ProduktBatchDTO> getProduktBatchList() throws DALException;
 	void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
 	void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
+	public void updateStatus(ProduktBatchDTO produktbatch) throws DALException;
 }
