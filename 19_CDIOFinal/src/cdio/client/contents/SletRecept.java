49e3f58f68c5fa5401032035f2b3723f5cb03e0a
JacobWorckJepsen <JacobWorckJepsen@10.16.167.252>
2015-06-11 14:15:03 +0200

    Tilføjelse: SletRecept - klasse

---
+++ 19_CDIOFinal/src/cdio/client/contents/SletRecept.java
@@ -0,0 +1,13 @@
+package cdio.client.contents;
+
+import cdio.client.ServiceAsync;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class SletRecept extends Composite {
+	
+	public SletRecept(String token, final ServiceAsync service){
+		
+	}
+
+}
