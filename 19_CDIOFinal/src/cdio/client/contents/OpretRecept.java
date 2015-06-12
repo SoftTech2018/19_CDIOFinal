4dd69980fabc98471684c22b3cfab9cd74fcd3d5
Camilla <owlebowle@10.16.174.171>
2015-06-12 10:12:27 +0200

    Rettelse: ændring i formattering af opretRecept

--- 19_CDIOFinal/src/cdio/client/contents/OpretRecept.java
+++ 19_CDIOFinal/src/cdio/client/contents/OpretRecept.java
@@ -20,21 +20,25 @@ import com.google.gwt.user.client.ui.*;
 
 public class OpretRecept extends Composite {
 
-	private VerticalPanel vPane;
-	private FlexTable ft;
+	private VerticalPanel vPane, vPane1;
+	private FlexTable ft, ft3;
 	private TextBox receptid, navn, raavareid, nomNetto, tolerance;
 	private Button opret, tilfoej, ok;
 	private Label error;
 	private boolean receptidValid, navnValid, nettoValid, tolValid, raavareidValid, kompValid, netEle, tolEle, ravEle;
 	private String[] split;
-
+	private HorizontalPanel hp;
+	
+	
 	public OpretRecept() {
-		vPane = new VerticalPanel();
-		initWidget(vPane);
+		hp = new HorizontalPanel();
+		initWidget(hp);
 		run();
 	}
 
 	private void run(){
+		vPane = new VerticalPanel();
+		vPane1 = new VerticalPanel();
 		receptidValid = true;
 		navnValid = false;
 		nettoValid = false;
@@ -45,7 +49,8 @@ public class OpretRecept extends Composite {
 		tolEle = false;
 		ravEle = false;
 
-		vPane.clear();
+		hp.clear();
+		
 
 		error = new Label("");
 		ft = new FlexTable();
@@ -55,7 +60,6 @@ public class OpretRecept extends Composite {
 
 		ft.setText(2, 0, "Receptnavn:");
 		navn = new TextBox();
-
 
 		navn.addKeyUpHandler(new NameCheck()); 
 		navn.setStyleName("TextBox-Opret");
@@ -71,24 +75,61 @@ public class OpretRecept extends Composite {
 		opret = new Button("Opret");
 		opret.addClickHandler(new OpretClick());
 		opret.setEnabled(false);
-		ft.setWidget(10, 1, opret);
+		ft.setWidget(10, 1, opret);		
+		
 
+		FlexTable ft2 = new FlexTable();
+		ok = new Button("Tilføj");
+		ft2.setText(1, 0, "RåvareID");
+		raavareid = new TextBox();
+		raavareid.addKeyUpHandler(new rIdCheck()); 
+		ft2.setWidget(1, 1, raavareid);
+		String rid = raavareid.getText();
+		if (rid != null){
+			ravEle = true;}
+
+
+		ft2.setText(0, 0, "Ny receptKomponent");
+			
+		ft2.setText(2, 0, "Nominel Netto");
+		nomNetto = new TextBox();
+		nomNetto.addKeyUpHandler(new nettoCheck());
+		String nNet = nomNetto.getText();
+		if(nNet != null){
+			netEle = true;
+		}
+		ft2.setWidget(2, 1, nomNetto);
+		
+		ft2.setText(3, 0, "Tolerance");
+		tolerance = new TextBox();
+		tolerance.addKeyUpHandler(new tolCheck());
+		String tol = tolerance.getText();
+		if(tol != null){
+			tolEle = true; 
+		}
+		ft2.setWidget(3, 1, tolerance);
+		
 		tilfoej = new Button("Ny komponent");
 		tilfoej.setStyleName("Recept-Komponenter");
 		tilfoej.addClickHandler(new kompClick());
-		ft.setWidget(4, 0, tilfoej);
+		ft2.setWidget(0, 1, tilfoej);
 
-		vPane.add(ft);
-		vPane.add(error);
-
-
+		ft2.getFlexCellFormatter().setWidth(0, 0, "200px");
+		ft2.getFlexCellFormatter().setWidth(0, 1, "200px");
+		ft2.getFlexCellFormatter().setWidth(0, 2, "200px");
+		ft2.getFlexCellFormatter().setWidth(0, 3, "200px");
+		ft2.getFlexCellFormatter().setWidth(0, 4, "200px");
+		
+		
+//		vPane.add(ft);		
+//		vPane1.add(ft2);
+		hp.add(error);
+		hp.add(ft);
+		hp.add(ft2);
+		
 	}
 
 	private class OpretClick implements ClickHandler{
-
-
-
-
 		@Override
 		public void onClick(ClickEvent event) {
 			opret.setEnabled(false);
@@ -98,6 +139,11 @@ public class OpretRecept extends Composite {
 					Integer.parseInt(raavareid.getText()),
 					Double.parseDouble(nomNetto.getText()),
 					Double.parseDouble(tolerance.getText()));
+
+			ReceptDTO recept = new ReceptDTO(
+					Integer.parseInt(receptid.getText()), 
+					navn.getText());
+
 
 			Controller.service.createReceptKomp(Controller.token, receptKomp, new AsyncCallback<Void>(){
 
@@ -113,30 +159,26 @@ public class OpretRecept extends Composite {
 				public void onSuccess(Void result) {
 
 
-					
 				}
 
 			});
-			
-			ReceptDTO recept = new ReceptDTO(
-					Integer.parseInt(receptid.getText()), 
-					navn.getText());
-			
+
+
 			Controller.service.createRecept(Controller.token, recept, new AsyncCallback<Void>(){
-				
+
 				@Override
 				public void onFailure(Throwable caught) {
 					opret.setEnabled(true);	
 					error.setText(caught.getMessage());
 					error.setStyleName("TextBox-ErrorMessage");
 				}
-				
+
 				@Override
 				public void onSuccess(Void result) {
 					Window.alert("Recept " + navn.getText() + " blev oprettet!");
 					run();
 				}
-				
+
 			} );
 
 		}
@@ -189,6 +231,7 @@ public class OpretRecept extends Composite {
 
 		@Override
 		public void onClick(ClickEvent event) {
+			opret.setEnabled(false);
 			FlexTable ft2 = new FlexTable();
 			ok = new Button("Tilføj");
 			ft2.setText(1, 0, "RåvareID");
@@ -230,7 +273,10 @@ public class OpretRecept extends Composite {
 			ft2.getFlexCellFormatter().setWidth(0, 2, "70px");
 			ft2.getFlexCellFormatter().setWidth(0, 3, "70px");
 			ft2.getFlexCellFormatter().setWidth(0, 4, "100px");
-			vPane.add(ft2);			
+			
+			
+			
+			
 
 
 		}
