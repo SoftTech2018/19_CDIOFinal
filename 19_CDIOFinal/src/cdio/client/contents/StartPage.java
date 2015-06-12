e995a4b4c955d4584538945fe55ca4c4ca372a0b
Ebbe <Ebbe@10.16.172.36>
2015-06-12 14:13:54 +0200

    Fix: Formateret startsiden

--- 19_CDIOFinal/src/cdio/client/contents/StartPage.java
+++ 19_CDIOFinal/src/cdio/client/contents/StartPage.java
@@ -8,6 +8,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -57,15 +58,19 @@ public class StartPage extends Composite {
 		ft.setWidget(0, 1, blue);
 		ft.setWidget(0, 2, red);
 		ft.setWidget(0, 3, green);
+		HTML gap = new HTML();
+		gap.setHTML("<br><br>");
 		
 		String vt = "Velkommen til det Distribueret Afvejningssystem. "
 				+ "Ude i menuen til venstre er det muligt at vælge, "
 				+ "hvilken funktion du vil benytte i system. "
 				+ "Lige ovenfor kan du vælge hvilken farve hjemmesiden skal have. "
 				+ "God fornøjelse med systemet!";
+		ft2.setStyleName("TextBox-Startside");
 		ft2.setText(0, 0, vt);
 		
 		vPane.add(ft);
+		vPane.add(gap);
 		vPane.add(ft2);
 		
 		
