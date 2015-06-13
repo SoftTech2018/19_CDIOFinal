764540f66722ed94d3873f38302cadd4da5dc21f
ministeren <ministeren@192.168.1.45>
2015-06-13 23:24:09 +0200

    Tilføjelse: opretPB begyndt

--- 19_CDIOFinal/src/cdio/client/contents/OpretRB.java
+++ 19_CDIOFinal/src/cdio/client/contents/OpretRB.java
@@ -3,11 +3,32 @@ package cdio.client.contents;
 import cdio.client.ServiceAsync;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class OpretRB extends Composite {
+	
+	private VerticalPanel vPane;
+	private FlexTable ft;
+	private TextBox rbID, raavareID, maengde;
 
 	public OpretRB() {
-		// TODO Auto-generated constructor stub
+		vPane = new VerticalPanel();
+		initWidget(vPane);
+		run();
+	}
+	
+	public void run(){
+		
+		ft = new FlexTable();
+		ft.setStyleName("FlexTable-Content");
+		ft.getRowFormatter().setStyleName(0, "FlexTable-Header");
+		ft.setText(0, 0, "Opret Råvare Batch");
+		
+		ft.setText(1, 0, "Opret Råvare Batch");
+		
+		
 	}
 
 }
