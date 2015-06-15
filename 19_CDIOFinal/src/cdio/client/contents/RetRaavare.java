d9382f64b7f4174d313fbf0390cafc297103ef56
JacobWorckJepsen <JacobWorckJepsen@10.16.174.194>
2015-06-15 14:00:16 +0200

    Tilføjelse: RetRaavare testet med TokenException

--- 19_CDIOFinal/src/cdio/client/contents/RetRaavare.java
+++ 19_CDIOFinal/src/cdio/client/contents/RetRaavare.java
@@ -6,6 +6,7 @@ import cdio.client.Controller;
 import cdio.client.ServiceAsync;
 import cdio.shared.FieldVerifier;
 import cdio.shared.RaavareDTO;
+import cdio.shared.TokenException;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -17,6 +18,7 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -135,13 +137,33 @@ public class RetRaavare  extends Composite {
 
 		@Override
 		public void onClick(ClickEvent event) {
+			
+			
+			
+			for (int i = 1; i<ft.getRowCount(); i++){
+				((Button) ft.getWidget(i, 3)).setEnabled(false);
+			}
+			((Button) ft.getWidget(eventRow, 4)).setEnabled(false);
+			
 			RaavareDTO raavare = new RaavareDTO(Integer.parseInt(((TextBox) ft.getWidget(eventRow, 0)).getText()), ((TextBox)ft.getWidget(eventRow, 1)).getText(), ((TextBox)ft.getWidget(eventRow, 2)).getText());
 			Controller.service.updateRaavare(Controller.token, raavare, new AsyncCallback<Void>(){
 
 				@Override
 				public void onFailure(Throwable caught) {
+					if (caught instanceof TokenException){
+						final PopupLogin pop = new PopupLogin();
+						pop.setPopupPositionAndShow(new PopupPanel.PositionCallback() {
+							public void setPosition(int offsetWidth, int offsetHeight) {
+								int left = (Window.getClientWidth() - offsetWidth) / 3;
+								int top = (Window.getClientHeight() - offsetHeight) / 3;
+								pop.setPopupPosition(left, top);
+							}
+						});
+					}
 					error.setText(caught.getMessage());
 					error.setStyleName("TextLabel-ErrorMessage");
+					openEventRow = 0;
+					run();
 				}
 
 				@Override
