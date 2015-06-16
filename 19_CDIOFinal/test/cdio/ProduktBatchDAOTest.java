5d33a4569e418314498cae14cab750cb2516c410
JacobWorckJepsen <JacobWorckJepsen@10.16.165.207>
2015-06-16 15:20:03 +0200

    Tilføjelse: ProduktBatchDAOTest næsten færdig

--- 19_CDIOFinal/test/cdio/ProduktBatchDAOTest.java
+++ 19_CDIOFinal/test/cdio/ProduktBatchDAOTest.java
@@ -1,6 +1,9 @@
 package cdio;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.SQLException;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,7 +11,16 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import cdio.server.DAL.Connector;
+import cdio.server.DAL.TextReader;
+import cdio.server.DAL.dao.ProduktBatchDAO;
+import cdio.server.DAL.idao.IProduktBatchDAO;
+import cdio.shared.DALException;
+import cdio.shared.ProduktBatchDTO;
+
 public class ProduktBatchDAOTest {
+	
+	IProduktBatchDAO pbDAO;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,6 +32,13 @@ public class ProduktBatchDAOTest {
 
 	@Before
 	public void setUp() throws Exception {
+		try { new Connector(); } 
+		catch (InstantiationException e) { e.printStackTrace(); }
+		catch (IllegalAccessException e) { e.printStackTrace(); }
+		catch (ClassNotFoundException e) { e.printStackTrace(); }
+		catch (SQLException e) { e.printStackTrace(); }
+		TextReader txt = new TextReader("war");
+		pbDAO = new ProduktBatchDAO(txt);
 	}
 
 	@After
@@ -27,13 +46,11 @@ public class ProduktBatchDAOTest {
 	}
 
 	@Test
-	public void testProduktBatchDAO() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testGetProduktBatch() {
-		fail("Not yet implemented");
+		try {
+			int pbID = pbDAO.getProduktBatch(1).getPbId();
+			assertEquals(1,pbID);
+		} catch (DALException e) {e.printStackTrace();}	
 	}
 
 	@Test
@@ -43,31 +60,48 @@ public class ProduktBatchDAOTest {
 
 	@Test
 	public void testCreateProduktBatch() {
-		fail("Not yet implemented");
+		int pbID = 0, temp = 0;
+		try {
+			for(cdio.shared.ProduktBatchDTO pbDto : pbDAO.getProduktBatchList()){
+				temp = pbDto.getPbId();
+				if (temp>pbID){
+					pbID = temp;
+				}				
+			}
+			pbID++;
+			pbDAO.createProduktBatch(new ProduktBatchDTO(pbID, 100+pbID, pbDAO.getProduktBatch(temp).getReceptId(), "16-06-2015", "16-06-2015", "16-06-2015"));
+			assertEquals(100+pbID, pbDAO.getProduktBatch(pbID).getStatus());
+		} catch (DALException e) {e.printStackTrace();}
 	}
 
 	@Test
 	public void testUpdateProduktBatch() {
-		fail("Not yet implemented");
+		int pbID = 1;
+		try {
+			for(ProduktBatchDTO pbDto : pbDAO.getProduktBatchList()){
+				if (String.valueOf(pbDto.getStatus()).startsWith("10")){
+					pbID = pbDto.getPbId();
+					break;
+				}				
+			}
+			pbDAO.updateProduktBatch(new ProduktBatchDTO(pbID, 500+pbID, pbDAO.getProduktBatch(pbID).getReceptId(), "16-06-2015", "16-06-2015", "16-06-2015"));
+			assertEquals(500+pbID, pbDAO.getProduktBatch(pbID).getStatus());
+		} catch (DALException e) {e.printStackTrace();}	
 	}
 
 	@Test
 	public void testUpdateStatus() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetPbViewList() {
-		fail("Not yet implemented");
+		try {
+			pbDAO.createProduktBatch(new ProduktBatchDTO(25, 0, 3, "16-06-2015", "16-06-2015", "16-06-2015"));
+			pbDAO.updateStatus(pbDAO.getLatestPbId(), 2);
+			assertEquals(2, pbDAO.getProduktBatch(pbDAO.getLatestPbId()).getStatus());
+		} catch (DALException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void testGetLatestPbId() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCheckReceptID() {
 		fail("Not yet implemented");
 	}
 
