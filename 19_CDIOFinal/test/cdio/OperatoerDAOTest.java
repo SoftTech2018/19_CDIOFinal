e87d6ebe55983e5ebb04d0bf7475933e1db8a997
JacobWorckJepsen <JacobWorckJepsen@10.16.165.207>
2015-06-16 14:23:53 +0200

    Tilføjelse: JUnit OperatoerDAOTest færdig

--- 19_CDIOFinal/test/cdio/OperatoerDAOTest.java
+++ 19_CDIOFinal/test/cdio/OperatoerDAOTest.java
@@ -17,14 +17,16 @@ import cdio.server.DAL.dao.OperatoerDAO;
 import cdio.server.DAL.dao.ReceptKompDAO;
 import cdio.server.DAL.idao.IOperatoerDAO;
 import cdio.shared.DALException;
+import cdio.shared.UserDTO;
 
 public class OperatoerDAOTest {
-	
+
 	IOperatoerDAO oprDAO;
 	TextReader txt;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+
 	}
 
 	@AfterClass
@@ -53,78 +55,128 @@ public class OperatoerDAOTest {
 			assertEquals(1,oprID);
 		} catch (DALException e) {
 			e.printStackTrace();
-			//hej
 		}
 	}
 
 	@Test
 	public void testCreateOperatoer() {
-		fail("Not yet implemented");
+		int oprID = 0, temp = 0;
+		try {
+			for(UserDTO oprDto : oprDAO.getOperatoerList()){
+				temp = oprDto.getUserId();
+				if (temp>oprID){
+					oprID = temp;	
+				}				
+			}
+			oprID++;
+			oprDAO.createOperatoer(new UserDTO(Integer.toString(oprID), "test"+Integer.toString(oprID), "test"+Integer.toString(oprID), "test"+Integer.toString(oprID), "test"+Integer.toString(oprID), false, true, false, false));
+			assertEquals("test"+oprID, oprDAO.getOperatoer(oprID).getName());
+		} catch (DALException e) {e.printStackTrace();}	
 	}
 
 	@Test
 	public void testUpdateOperatoer() {
-		fail("Not yet implemented");
+		int oprID = 0;
+		try {
+			for(UserDTO oprDto : oprDAO.getOperatoerList()){
+				if (oprDto.getName().startsWith("test")){
+					oprID = oprDto.getUserId();
+					break;
+				}				
+			}
+			oprDAO.updateOperatoer(new UserDTO(Integer.toString(oprID), "update test"+Integer.toString(oprID), "update test"+Integer.toString(oprID), "update test"+Integer.toString(oprID), "update test"+Integer.toString(oprID), false, true, false, false));
+			assertEquals("update test"+oprID, oprDAO.getOperatoer(oprID).getName());
+		} catch (DALException e) {e.printStackTrace();}		
 	}
 
 	@Test
 	public void testGetOperatoerList() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetListViewOpr() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetView() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetProcedure() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCallProcedure() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetFunction() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetFunction() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDropAll() {
-		fail("Not yet implemented");
+		try {
+			boolean oprList = oprDAO.getOperatoerList().isEmpty();
+			assertEquals(false, oprList);
+		} catch (DALException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void testGetAdminCount() {
-		fail("Not yet implemented");
-	}
+		try {
+			int oprID = 0, temp = 0;
+				for(UserDTO oprDto : oprDAO.getOperatoerList()){
+					temp = oprDto.getUserId();
+					if (temp>oprID){
+						oprID = temp;	
+					}				
+				}
+				oprID++;
+				int admCount = oprDAO.getAdminCount();
+				oprDAO.createOperatoer(new UserDTO(Integer.toString(oprID), "admin"+Integer.toString(oprID), "admin"+Integer.toString(oprID), "admin"+Integer.toString(oprID), "admin"+Integer.toString(oprID), true, false, false, false));
+				assertEquals(admCount+1, oprDAO.getAdminCount());
+			} catch (DALException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
-	@Test
-	public void testGetFarmaceutCount() {
-		fail("Not yet implemented");
-	}
+		@Test
+		public void testGetFarmaceutCount() {
+			try {
+				int oprID = 0, temp = 0;
+					for(UserDTO oprDto : oprDAO.getOperatoerList()){
+						temp = oprDto.getUserId();
+						if (temp>oprID){
+							oprID = temp;	
+						}				
+					}
+					oprID++;
+					int farmCount = oprDAO.getFarmaceutCount();
+					oprDAO.createOperatoer(new UserDTO(Integer.toString(oprID), "farm"+Integer.toString(oprID), "farm"+Integer.toString(oprID), "farm"+Integer.toString(oprID), "farm"+Integer.toString(oprID), false, true, false, false));
+					assertEquals(farmCount+1, oprDAO.getFarmaceutCount());
+				} catch (DALException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
 
-	@Test
-	public void testGetVaerkfoererCount() {
-		fail("Not yet implemented");
-	}
+		@Test
+		public void testGetVaerkfoererCount() {
+			try {
+				int oprID = 0, temp = 0;
+					for(UserDTO oprDto : oprDAO.getOperatoerList()){
+						temp = oprDto.getUserId();
+						if (temp>oprID){
+							oprID = temp;	
+						}				
+					}
+					oprID++;
+					int varkCount = oprDAO.getVaerkfoererCount();
+					oprDAO.createOperatoer(new UserDTO(Integer.toString(oprID), "vark"+Integer.toString(oprID), "vark"+Integer.toString(oprID), "vark"+Integer.toString(oprID), "vark"+Integer.toString(oprID), false, false, true, false));
+					assertEquals(varkCount+1, oprDAO.getVaerkfoererCount());
+				} catch (DALException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
 
-	@Test
-	public void testGetOperatoerCount() {
-		fail("Not yet implemented");
-	}
+		@Test
+		public void testGetOperatoerCount() {
+			try {
+				int oprID = 0, temp = 0;
+					for(UserDTO oprDto : oprDAO.getOperatoerList()){
+						temp = oprDto.getUserId();
+						if (temp>oprID){
+							oprID = temp;	
+						}				
+					}
+					oprID++;
+					int oprCount = oprDAO.getOperatoerCount();
+					oprDAO.createOperatoer(new UserDTO(Integer.toString(oprID), "opr"+Integer.toString(oprID), "opr"+Integer.toString(oprID), "opr"+Integer.toString(oprID), "opr"+Integer.toString(oprID), false, false, false, true));
+					assertEquals(oprCount+1, oprDAO.getOperatoerCount());
+				} catch (DALException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
 
-}
+	}
