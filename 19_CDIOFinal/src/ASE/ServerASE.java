97ea68bd92606bb75fcbb5cf163095ff344e9374
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2015-06-17 18:27:52 +0200

    Formatering

--- 19_CDIOFinal/src/ASE/ServerASE.java
+++ 19_CDIOFinal/src/ASE/ServerASE.java
@@ -14,13 +14,13 @@ import cdio.shared.DALException;
 
 public class ServerASE implements Runnable{
 
-	private List<String> ip;
-	static List<Thread> active;
+	private List<String> ipList;
+	private List<Thread> active;
 	private List<String> activeIp;
 	private int port, portCheck;
 
 	public ServerASE(){
-		ip = new ArrayList<String>();
+		ipList = new ArrayList<String>();
 		active = new ArrayList<Thread>();
 		activeIp = new ArrayList<String>();
 		port = 8000; // Hvilken port benyttes til afvejningsproceduren
@@ -31,9 +31,9 @@ public class ServerASE implements Runnable{
 		 */
 		//		ip.add("169.254.2.2");
 		//		ip.add("169.254.2.3");
-		ip.add("192.168.1.24");
-		ip.add("localHost");
-		ip.add("192.168.1.213");
+		ipList.add("192.168.1.24");
+		ipList.add("localHost");
+		ipList.add("192.168.1.213");
 	}
 
 	@Override
@@ -48,13 +48,13 @@ public class ServerASE implements Runnable{
 					 */
 					if (!active.get(p).isAlive()){
 						System.out.println("Afvejningsprocedure afsluttet på ip: " + activeIp.get(p));
-						ip.add(activeIp.get(p)); 
+						ipList.add(activeIp.get(p)); 
 						active.remove(p);
 						activeIp.remove(p);
 					}
 				}
 
-				for (int i=0; i<ip.size(); i++){
+				for (int i=0; i<ipList.size(); i++){
 					/*
 					 *  Tjek om der kan oprettes forbindelse til IP på port 7 (echo). 
 					 *  I så fald oprettes en afvejningsprocedure på vægten.
@@ -62,9 +62,9 @@ public class ServerASE implements Runnable{
 					Socket w = null;
 					try {
 						w = new Socket();
-						w.connect(new InetSocketAddress(ip.get(i), portCheck), 1000);  // 1000 = 1 sek til at connecte
+						w.connect(new InetSocketAddress(ipList.get(i), portCheck), 1000);  // 1000 = 1 sek til at connecte
 					} catch (SocketTimeoutException e){
-						System.out.println("Kunne ikke forbinde til: " + ip.get(i));
+						System.out.println("Kunne ikke forbinde til: " + ipList.get(i));
 					}
 					if (w.isConnected()){
 						w.close(); // Luk socket så ProcedureController kan oprette sin egen socket
@@ -72,14 +72,14 @@ public class ServerASE implements Runnable{
 						ITransmitter trans = new Transmitter();
 						IControllerDAO dao = new ControllerDAO(0);
 
-						IProcedureController ase = new ProcedureController(menu, dao , ip.get(i), port, trans);
+						IProcedureController ase = new ProcedureController(menu, dao , ipList.get(i), port, trans);
 						Thread t = new Thread((Runnable) ase);
 						t.start();
 
 						active.add(t);
-						activeIp.add(ip.get(i));
-						System.out.println("Afvejningsprocedure startet på ip: " + ip.get(i));
-						ip.remove(i); 
+						activeIp.add(ipList.get(i));
+						System.out.println("Afvejningsprocedure startet på ip: " + ipList.get(i));
+						ipList.remove(i); 
 					}
 				}
 			} catch (IOException e) {
